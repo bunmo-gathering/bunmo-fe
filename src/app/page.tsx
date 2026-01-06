0fd13e7c088a001da749dd401ac0aebf675ae7f6
seyun0714 <seun0714@naver.com>
2026-01-06 21:18:34 +0900

    test: Radio 컴포넌트 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,16 +27,24 @@ export default function Home() {
 
       <div>
         <Radio
+          id="백종원"
           name={"이름"}
           value={"백종원"}
           selectedValue={selectedValue}
           handleChange={(e) => setSelectedValue(e.target.value)}
+          type="first"
+          label="백종원"
+          description="백종원의 요리비책"
         />
         <Radio
+          id="박종원"
           name={"이름"}
           value={"박종원"}
           selectedValue={selectedValue}
           handleChange={(e) => setSelectedValue(e.target.value)}
+          type="second"
+          label="박종원"
+          description="박종원의 코딩비책"
         />
       </div>
     </>
