075d87252105fe6f0a13308a34322ebb64ee963f
seyun0714 <seun0714@naver.com>
2026-01-11 16:50:43 +0900

    feat: Pick 컴포넌트 구현

---
+++ src/components/Picker/Pick/animate.ts
@@ -0,0 +1,13 @@
+import { Animate, Transition } from "@/types/animate";
+
+const PickAnimation = {
+  whileTap: {
+    scale: 0.98,
+  } as Animate,
+
+  transition: {
+    duration: 0.1,
+  } as Transition,
+};
+
+export default PickAnimation;
