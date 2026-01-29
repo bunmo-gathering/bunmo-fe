d54b9a7444e4b43521f0933554f98c5d8c8bf15a
Jongwon Park <pjw970220@gmail.com>
2026-01-30 00:41:06 +0900

    chore: event 설명 추가

--- src/constants/event.ts
+++ src/constants/event.ts
@@ -1,3 +1,15 @@
-type BridgeEventToken = "NONE" | "OPEN_MODAL_CONFIRM" | "OPEN_MODAL_ALERT";
+{
+  /**
+   * EventToken은 `Action_Target[_Modifier]` 형식으로 정의됩니다.
+   *
+   * - Action: 이벤트의 동작을 나타냅니다. (예: OPEN, CLOSE, UPDATE 등)
+   * - Target: 이벤트가 작용하는 대상을 나타냅니다. (예: MODAL, BOTTOMSHEET, DATA 등)
+   * - Modifier (선택 사항): 이벤트의 특수한 속성이나 조건을 나타냅니다. (예: CONFIRM, ALERT 등)
+   */
+}
 
-export type { BridgeEventToken };
+type BridgePostEventToken = "NONE" | "OPEN_MODAL_CONFIRM" | "OPEN_MODAL_ALERT";
+
+type BridgeGetEventToken = "ACTION_CONFIRMED" | "ACTION_CANCELED";
+
+export type { BridgePostEventToken };
