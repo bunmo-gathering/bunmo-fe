09e6499d0db02fd8a5bf21c13f7b0f559372b2e6
seyun0714 <seun0714@naver.com>
2026-02-16 16:04:25 +0900

    feat: AvatarLabelCard 컴포넌트 구현

---
+++ src/components/AvatarLabelCard/props.type.ts
@@ -0,0 +1,9 @@
+import { UserInfo } from "../UserGroup/props.type";
+
+interface AvatarLabelCardProps {
+  title: string;
+  entryUsers: UserInfo[];
+  maxEntry: number;
+}
+
+export type { AvatarLabelCardProps };
